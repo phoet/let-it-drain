76d51820065b4ff58f45f666b1cd40939f94a2fb
phoet <phoetmail@googlemail.com>
2015-05-10 13:46:43 +0200

    nicer message

--- use_let_it_drain.rb
+++ use_let_it_drain.rb
@@ -1,6 +1,7 @@
 require 'bundler'
 Bundler.require(:default, :development)
 
-puts ENV['LET_IT_DRAIN_URL']
-log = Logglier.new(ENV['LET_IT_DRAIN_URL'])
-log.warn boom: :box, bar: :soap
+puts url = ENV['LET_IT_DRAIN_URL']
+log = Logglier.new(url)
+
+3.times { log.warn "so WOW much LOG!" }
